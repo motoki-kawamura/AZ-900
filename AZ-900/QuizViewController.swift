40a30a4b707c23cfadb970b8028d24b2e4e87e27
motoki-kawamura <813motoki2072369@gmail.com>
2022-11-23 16:27:54 +0900

    version1.5

--- AZ-900/QuizViewController.swift
+++ AZ-900/QuizViewController.swift
@@ -18,6 +18,7 @@ class QuizViewController: UIViewController {
     var csvArray: [String] = []
     var quizArray: [String] = []
     var quizCount = 0
+    var correctCount = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,9 +36,37 @@ class QuizViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let scoreVC = segue.destination as! ScoreViewController
+        scoreVC.correct = correctCount
+    }
+    
     //ボタンを押した時に呼ばれる
     @IBAction func btnAction(sender: UIButton){
-        print(sender.tag)
+        if sender.tag == Int(quizArray[1]){
+            correctCount += 1
+            print("正解")
+        } else{
+            print("不正解")
+        }
+        print("スコア：\(correctCount)")
+        nextQuiz()
+    }
+    
+    func nextQuiz(){
+        quizCount += 1
+        if quizCount < csvArray.count{
+            quizArray = csvArray[quizCount].components(separatedBy: ",")
+            quizNumberLabel.text = "第\(quizCount + 1)問"
+            quizTextView.text = quizArray[0]
+            answerButton1.setTitle(quizArray[2], for: .normal)
+            answerButton2.setTitle(quizArray[3], for: .normal)
+            answerButton3.setTitle(quizArray[4], for: .normal)
+            answerButton4.setTitle(quizArray[5], for: .normal)
+        } else {
+            performSegue(withIdentifier: "toScoreVC", sender: nil)
+        }
     }
     
     func loadCSV(fileName: String) -> [String]{
