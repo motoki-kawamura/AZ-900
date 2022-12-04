7410c73a0a624d80d3982b834906be6275c30cdb
motoki-kawamura <813motoki2072369@gmail.com>
2022-12-04 14:24:01 +0900

    version 1.7

--- AZ-900/QuizViewController.swift
+++ AZ-900/QuizViewController.swift
@@ -14,6 +14,7 @@ class QuizViewController: UIViewController {
     @IBOutlet var answerButton2: UIButton!
     @IBOutlet var answerButton3: UIButton!
     @IBOutlet var answerButton4: UIButton!
+    @IBOutlet var judgeImageView: UIImageView!
     
     var csvArray: [String] = []
     var quizArray: [String] = []
@@ -47,11 +48,25 @@ class QuizViewController: UIViewController {
         if sender.tag == Int(quizArray[1]){
             correctCount += 1
             print("正解")
+            judgeImageView.image = UIImage(named: "correct")
         } else{
             print("不正解")
+            judgeImageView.image = UIImage(named: "incorrect")
         }
         print("スコア：\(correctCount)")
-        nextQuiz()
+        judgeImageView.isHidden = false
+        answerButton1.isEnabled = false
+        answerButton2.isEnabled = false
+        answerButton3.isEnabled = false
+        answerButton4.isEnabled = false
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5){
+            self.judgeImageView.isHidden = true
+            self.answerButton1.isEnabled = true
+            self.answerButton2.isEnabled = true
+            self.answerButton3.isEnabled = true
+            self.answerButton4.isEnabled = true
+            self.nextQuiz()
+        }
     }
     
     func nextQuiz(){
